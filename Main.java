4567f59df2d737b30beb5c65cdc6a3d90970de8a
MarHovsepyan <124073403+MarHovsepyan@users.noreply.github.com>
2023-01-31 20:20:45 +0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.println("Ներմուծեք առաջին տարրը");
+        double arajinTarr = console.nextDouble();
+        double patasxan = arajinTarr;
+        int gorcoxutyun;
+        do {
+            System.out.println("Ներմուծեք գործողությունը");
+            System.out.println("0֊STOP");
+            System.out.println("1֊գումարում");
+            System.out.println("2֊հանում");
+            System.out.println("3֊բազմապատկում");
+            System.out.println("4֊բաժանում");
+
+          gorcoxutyun = console.nextInt();
+        System.out.println("Ներմուծեք մյուս տարրը");
+        double erkrordTarr = console.nextDouble();
+
+        switch (gorcoxutyun) {
+            case 0:
+                System.out.println("ավարտ");
+                break;
+            case 1:
+                patasxan = patasxan + erkrordTarr;
+                break;
+            case 2:
+                patasxan = patasxan- erkrordTarr;
+                break;
+            case 3:
+                patasxan = patasxan * erkrordTarr;
+                break;
+            case 4:
+                patasxan = patasxan/ erkrordTarr;
+                break;
+            default:
+                System.out.println("Փորձեք ևս մեկ անգամ");
+        }
+        System.out.print("Պատասխանը=");
+        System.out.println(patasxan);
+
+    }
+        while( gorcoxutyun!=0);
+    }
+}
